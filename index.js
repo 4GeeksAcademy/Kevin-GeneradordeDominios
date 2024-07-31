d69487209d26affc2cfdbcad9d7c3f871e39a68a
Kevin Villafuerte Montero <116960904+Kevin962022@users.noreply.github.com>
2024-07-31 21:27:12 +0000

    corregido

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 window.onload = () => {
     document.querySelector("#generador").innerHTML = generadorDominios();
-    document.querySelector("#generador2").innerHTML = generadorDominios();
-    document.querySelector("#generador3").innerHTML = generadorDominios();
-    document.querySelector("#generador4").innerHTML = generadorDominios();
-    document.querySelector("#generador5").innerHTML = generadorDominios();
-    document.querySelector("#generador6").innerHTML = generadorDominios();
-    document.querySelector("#generador7").innerHTML = generadorDominios();
-    document.querySelector("#generador8").innerHTML = generadorDominios();
   };
 
 function generadorDominios() {
-    let pronoun = ['the', 'our'];
-    let adj = ['great', 'big'];
-    let noun = ['jogger', 'racoon'];
-    let extension = ['.com', '.es', '.mx',];
+   //declaracion de variables
+   let direccion = {
+      world: "www."
+   }
+   let pronoun = ['the', 'our'];
+   let adj = ['great', 'big'];
+   let noun = ['jogger', 'racoon']
+   let extension = ['.com', '.es','.us', '.io']
+   let container = [];
 
-    let pronounConsola = Math.floor(Math.random() * pronoun.length);
-    let adjConsola = Math.floor(Math.random() * adj.length);
-    let nounConsola = Math.floor(Math.random() * noun.length);
-    let extensionConsola = Math.floor(Math.random() * extension.length);
-
-    return pronoun[pronounConsola] + adj[adjConsola] + noun[nounConsola] + extension[extensionConsola];
-
+      for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+          for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < extension.length; l++) {
+              container.push(direccion.world + pronoun[i] + adj[j] + noun[k] + extension[l]);
+           }    
+        }
+      }
+   }
+   return container.join("<br>")
 }
-  for (let index = 0; index < 8; index++) {
-    console.log(generadorDominios() + ".com");
-  }
